8b7fa49306e4775bbe47f47e8899f9c8749d24a0
logan-james <loganjames448@gmail.com>
2024-07-10 17:39:07 -0700

    First sample script

---
+++ directory_listing.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# A simple script to output the files in the current directory
+# Author: logan-james
+# Date: July 10, 2024
+
+echo Your current directory is:
+pwd
+
+echo The files in your current directory are:
+ls
+
+#run script by ./directory_listing.sh
